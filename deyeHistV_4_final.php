c33a9c671aadea74f4fcf468dd83c1b06ecab7b3
Mandar <mandar@vmechatronics.com>
2025-12-02 16:54:34 +0530

    this is the latest push

--- deyeHistV_4_final.php
+++ deyeHistV_4_final.php
@@ -337,3 +337,71 @@ try {
 
 exit();
 ?>
+
+
+
+<!-- 
+curl -X POST "http://localhost:8000/deye_api.php" \
+     -H "Content-Type: application/json" \
+     -H "Token: Token IdeYNazjrEtOVbfPPTh8GXXhkZ" \
+     -d '{
+       "stationId": 61195166,
+       "accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2F1dGgyLXJlc291cmNlIl0sInVzZXJfbmFtZSI6IjBfcHVyY2hhc2VAdm1lY2hhdHJvbmljcy5jb21fMiIsInNjb3BlIjpbImFsbCJdLCJkZXRhaWwiOnsib3JnYW5pemF0aW9uSWQiOjAsInRvcEdyb3VwSWQiOm51bGwsImdyb3VwSWQiOm51bGwsInJvbGVJZCI6LTEsInVzZXJJZCI6MTMwMTYyNDYsInZlcnNpb24iOjEsImlkZW50aWZpZXIiOiJwdXJjaGFzZUB2bWVjaGF0cm9uaWNzLmNvbSIsImlkZW50aXR5VHlwZSI6MiwibWRjIjoiZXUiLCJhcHBJZCI6IjIwMjQxMTI2MzUzNTAwMiIsIm1mYVN0YXR1cyI6bnVsbCwidGVuYW50IjoiRGV5ZSJ9LCJleHAiOjE3Njk4NDA0NTMsIm1kYyI6ImV1IiwiYXV0aG9yaXRpZXMiOlsiYWxsIl0sImp0aSI6IjQ4Njc1ZjMxLWJiYjgtNGQwOS05NjBkLTZkN2RlZDYyMGFkNyIsImNsaWVudF9pZCI6InRlc3QifQ.M_Osxpdi8hKgDkszjxzRiB4fHoGMMA2z4WKukGZRZym9v8v_t3q0Vikw3OhWTBy7-7Y6TITMw6srYMUokwjSaHMQS4br90DIGGYHMDjIpfEGmqAgWXjq8Ac9A_DN7jZEywbwAD329w4hpWUAzVeEnrsS0OsXxBzxrtJCuW831U9fGv-WIQ3uLlx01VfUk411-ly5ocTo9JETB9yxFJIDoN-1aY0CiziPX45ufbid_WTEtSk_toX50Jc-ZG9aS74GUD7DBkJjZbjqKNC3vFCBIz4V_k5WGiBDGtwGKTg5sVm72EwuKtPkZRhGYUZGbCBiO2C7p2uIqGCVZpB3fp42xw",
+       "start": "2025-01-01T00:00:00Z",
+       "end": "2025-01-02T00:00:00Z"
+     }'
+	 
+curl -X POST "http://localhost:8000/deyehist4hhmm.php" \
+  -H "Content-Type: application/json" \
+  -H "Token: Token IdeYNazjrEtOVbfPPTh8GXXhkZ" \
+  -d '{
+    "stationId": "61195166",
+    "accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2F1dGgyLXJlc291cmNlIl0sInVzZXJfbmFtZSI6IjBfYnVkZGhhYmh1c2hhbkB2bWVjaGF0cm9uaWNzLmNvbV8yIiwic2NvcGUiOlsiYWxsIl0sImRldGFpbCI6eyJvcmdhbml6YXRpb25JZCI6MCwidG9wR3JvdXBJZCI6bnVsbCwiZ3JvdXBJZCI6bnVsbCwicm9sZUlkIjotMSwidXNlcklkIjoxMzQwMDQ4NSwidmVyc2lvbiI6MTAwMCwiaWRlbnRpZmllciI6ImJ1ZGRoYWJodXNoYW5Adm1lY2hhdHJvbmljcy5jb20iLCJpZGVudGl0eVR5cGUiOjIsIm1kYyI6ImV1IiwiYXBwSWQiOiIyMDI1MTAyNzY0ODYwMTAiLCJtZmFTdGF0dXMiOm51bGwsInRlbmFudCI6IkRleWUifSwiZXhwIjoxNzY5ODUzMDExLCJtZGMiOiJldSIsImF1dGhvcml0aWVzIjpbImFsbCJdLCJqdGkiOiJiODhkMzIyZi04ZjE0LTQ5NzUtOTNmYy00NzU2OGY0YWEzZWQiLCJjbGllbnRfaWQiOiJ0ZXN0In0.MALOPikYEM6DM0Wc2olYaGDxYJxpFY2p0Zf3xbZemw1pUFGJXyfZO9IlBq0WK0I7j9n-saD01LFxt930IzSn27-Dq2Au6ql7G0TrEV5WpAtWQ4wZhP2xCFyL5wkWbNlemadjiH_1nwUR75_r1sR27OPfEGJ-Q0PiurRGUI6ka1czCRxw5J1XCa2tjz5MH9bsd7w1cT5uYO4wWr_Ump9bt_Ijuv_MwXd-3XCev51_xXxJZ4_rfslUQpu8MibDp2srpzdlOFdc2g4kh046-2Ppf5cd9mRjpcAiY_JNnZu07BY2uewPI5gex2EKWSZfw1_Om0kQ1t5Rtp7Uqhj5q-VZKQ",
+    "start": "2025-11-29T00:00:00Z",
+    "end": "2025-11-30T00:00:00Z"
+  }'
+  
+ 
+ 
+     curl -X POST "http://localhost:8000/deyeHistV_4_final.php" \
+      -H "Content-Type: application/json" \
+      -H "Token: Token IdeYNazjrEtOVbfPPTh8GXXhkZ" \
+      -d '{
+        "stationId": "61195166",
+		"accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2F1dGgyLXJlc291cmNlIl0sInVzZXJfbmFtZSI6IjBfYnVkZGhhYmh1c2hhbkB2bWVjaGF0cm9uaWNzLmNvbV8yIiwic2NvcGUiOlsiYWxsIl0sImRldGFpbCI6eyJvcmdhbml6YXRpb25JZCI6MCwidG9wR3JvdXBJZCI6bnVsbCwiZ3JvdXBJZCI6bnVsbCwicm9sZUlkIjotMSwidXNlcklkIjoxMzQwMDQ4NSwidmVyc2lvbiI6MTAwMCwiaWRlbnRpZmllciI6ImJ1ZGRoYWJodXNoYW5Adm1lY2hhdHJvbmljcy5jb20iLCJpZGVudGl0eVR5cGUiOjIsIm1kYyI6ImV1IiwiYXBwSWQiOiIyMDI1MTAyNzY0ODYwMTAiLCJtZmFTdGF0dXMiOm51bGwsInRlbmFudCI6IkRleWUifSwiZXhwIjoxNzY5ODU3NTQ2LCJtZGMiOiJldSIsImF1dGhvcml0aWVzIjpbImFsbCJdLCJqdGkiOiJkMTE1MGZkZS02NmM0LTQ3YTktODI0MS00YTYzZGY0MzJiNWUiLCJjbGllbnRfaWQiOiJ0ZXN0In0.OlbggLWxYOd-sEB8sP-zkicKu_3mo_O-km29bx0xJAhZUljFNOX5K1vAyM6RwqI-eg1K41R5Yy5cAIvRhuHYmAT4fryb7rr9GWHF7YTuFsJa_wZBnHGh4p7tdNULX-KXoHSu3nWgRA62fGwLH89PaUhwZ6jR9g3ghnFbTYtV7-fIcP69DPM5Z2IdbxSJ9iJxiCyzDtK_JMST9S-b6ianzyEejVMZiKz3ea-uyJsYaddn50z-6IBZQ3QpC8FmTZfC2B0m_SdK6ezV6tESZQkLf3HUSN4ZCDqhS3JH7XCaczdvBlwfSW6wyyGtAad_Aal4KqQk2SpyadK5wVsgAl0zDw",
+		"start": "2025-11-01T00:00:00Z",
+		  "end": "2025-11-02T00:00:00Z"  
+      }'
+	  
+	  curl -X POST "http://localhost:8000/deyehist4hhmm.php" \
+  -H "Content-Type: application/json" \
+  -H "Token: Token IdeYNazjrEtOVbfPPTh8GXXhkZ" \
+  -d '{
+    "stationId": "61336113",
+    "accessToken": "eyJ...[your full token]",
+    "start": "2024-11-15T00:00:00Z",
+    "end": "2024-11-16T00:00:00Z"
+  }'
+  
+  
+  curl -X POST "http://localhost:8000/deye1.php" \
+  -H "Content-Type: application/json" \
+  -H "Token: Token IdeYNazjrEtOVbfPPTh8GXXhkZ" \
+  -d '{
+    "stationId": "61195166",
+    "accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2F1dGgyLXJlc291cmNlIl0sInVzZXJfbmFtZSI6IjBfYnVkZGhhYmh1c2hhbkB2bWVjaGF0cm9uaWNzLmNvbV8yIiwic2NvcGUiOlsiYWxsIl0sImRldGFpbCI6eyJvcmdhbml6YXRpb25JZCI6MCwidG9wR3JvdXBJZCI6bnVsbCwiZ3JvdXBJZCI6bnVsbCwicm9sZUlkIjotMSwidXNlcklkIjoxMzQwMDQ4NSwidmVyc2lvbiI6MTAwMCwiaWRlbnRpZmllciI6ImJ1ZGRoYWJodXNoYW5Adm1lY2hhdHJvbmljcy5jb20iLCJpZGVudGl0eVR5cGUiOjIsIm1kYyI6ImV1IiwiYXBwSWQiOiIyMDI1MTAyNzY0ODYwMTAiLCJtZmFTdGF0dXMiOm51bGwsInRlbmFudCI6IkRleWUifSwiZXhwIjoxNzY5ODU2NzA2LCJtZGMiOiJldSIsImF1dGhvcml0aWVzIjpbImFsbCJdLCJqdGkiOiJjZGRlNGEyNS0zYTIwLTRjNzQtYmNlNy1mZDgyNmJlMGJlMGUiLCJjbGllbnRfaWQiOiJ0ZXN0In0.Uk7dKX7iciCgapNvxUGKBmR5m0o8wxW5lsFesEbpy1cr6vY9m_lhMoLsBObzLM0gCkud0juIU8Q5tty2zqnF4sg9BlmU7kE2mspA-fhKv6OuEmJi45gIcb809wXEddEm1i4ZiJ6-2bKBDSsInnHe8bMO7QKiacl--u4ccjO0eN2e3gfDIyaYQPHYnb65F35fgUqDx50RT4uR767Flbt3wsEgPEIu0_-DoxGJDpD8ApaImrg7GtrjyOT_wTKnRdLmg6kp6VEP0S6aq81s72BBaA1DfqtnVqeJsK8sj80YBeDOttPAWlkzk8iPao5ynULF2fi9gbu7kg2ve0Wf6JvBcA",
+    "start": "2025-11-15T00:00:00Z",
+    "end": "2025-11-16T00:00:00Z"
+  }'
+  
+curl -X POST "http://localhost:8000/deyeHistV_3.php" \
+  -H "Content-Type: application/json" \
+  -H "Token: Token IdeYNazjrEtOVbfPPTh8GXXhkZ" \
+  -d '{
+    "stationId": "61195166",
+    "accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2F1dGgyLXJlc291cmNlIl0sInVzZXJfbmFtZSI6IjBfYnVkZGhhYmh1c2hhbkB2bWVjaGF0cm9uaWNzLmNvbV8yIiwic2NvcGUiOlsiYWxsIl0sImRldGFpbCI6eyJvcmdhbml6YXRpb25JZCI6MCwidG9wR3JvdXBJZCI6bnVsbCwiZ3JvdXBJZCI6bnVsbCwicm9sZUlkIjotMSwidXNlcklkIjoxMzQwMDQ4NSwidmVyc2lvbiI6MTAwMCwiaWRlbnRpZmllciI6ImJ1ZGRoYWJodXNoYW5Adm1lY2hhdHJvbmljcy5jb20iLCJpZGVudGl0eVR5cGUiOjIsIm1kYyI6ImV1IiwiYXBwSWQiOiIyMDI1MTAyNzY0ODYwMTAiLCJtZmFTdGF0dXMiOm51bGwsInRlbmFudCI6IkRleWUifSwiZXhwIjoxNzY5ODU3NTQ2LCJtZGMiOiJldSIsImF1dGhvcml0aWVzIjpbImFsbCJdLCJqdGkiOiJkMTE1MGZkZS02NmM0LTQ3YTktODI0MS00YTYzZGY0MzJiNWUiLCJjbGllbnRfaWQiOiJ0ZXN0In0.OlbggLWxYOd-sEB8sP-zkicKu_3mo_O-km29bx0xJAhZUljFNOX5K1vAyM6RwqI-eg1K41R5Yy5cAIvRhuHYmAT4fryb7rr9GWHF7YTuFsJa_wZBnHGh4p7tdNULX-KXoHSu3nWgRA62fGwLH89PaUhwZ6jR9g3ghnFbTYtV7-fIcP69DPM5Z2IdbxSJ9iJxiCyzDtK_JMST9S-b6ianzyEejVMZiKz3ea-uyJsYaddn50z-6IBZQ3QpC8FmTZfC2B0m_SdK6ezV6tESZQkLf3HUSN4ZCDqhS3JH7XCaczdvBlwfSW6wyyGtAad_Aal4KqQk2SpyadK5wVsgAl0zDw",
+    "start": "20/11/2025",
+    "end": "20/12/2025"
+  }' -->
+  
+  
